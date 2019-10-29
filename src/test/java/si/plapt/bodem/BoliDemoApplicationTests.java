47857bd6faf80e089823be94608aad48d22465ad
Bogdan Lipuš <bolipus@gmail.com>
2019-10-29 21:01:59 +0100

    Add spring boot

---
+++ src/test/java/si/plapt/bodem/BoliDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package si.plapt.bodem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoliDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
